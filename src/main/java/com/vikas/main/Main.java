939ded4788953e1fbc6e1c8eac57fe81274e846d
vikas <moganevikas@gmail.com>
2021-04-09 00:02:26 +0530

    3 annotation replaced by s1 springbootapplication annotaion and tested on chrome

--- src/main/java/com/vikas/main/Main.java
+++ src/main/java/com/vikas/main/Main.java
@@ -1,10 +1,17 @@
 package com.vikas.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//@SpringBootApplication
+
+@EnableAutoConfiguration
+@ComponentScan
+@SpringBootConfiguration
 public class Main {
 	
 public static void main(String[] args) {
